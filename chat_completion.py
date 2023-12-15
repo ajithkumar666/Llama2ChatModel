f70a21c81052e6c3649271ce371834c47bc8e9f4
Ajithkumar M <ajith721@gmail.com>
2023-12-15 18:22:22 +0530

    response format updated

--- chat_completion.py
+++ chat_completion.py
@@ -1,16 +1,11 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
-
 from typing import Optional
-
 import fire
-
 from llama import Llama
 
 
 def main(
-    ckpt_dir: str,
-    tokenizer_path: str,
+    ckpt_dir: str = "/home/ubuntu/llama/Llama2ChatModel/llama-2-7b-chat/",
+    tokenizer_path: str = "/home/ubuntu/llama/Llama2ChatModel/tokenizer.model",
     temperature: float = 0.6,
     top_p: float = 0.9,
     max_seq_len: int = 512,
@@ -24,15 +19,15 @@ def main(
         max_seq_len=max_seq_len,
         max_batch_size=max_batch_size,
     )
-    
+
     if len(prompt)==0:
-        print("Prompt is empty")
+        print("ImmiResult:Prompt is empty",)
     else:
-        print("Prompt is : ",prompt)
+        pass
 
     dialogs = [
         [{"role": "user", "content": prompt}],
-        
+
     ]
     results = generator.chat_completion(
         dialogs,  # type: ignore
@@ -42,13 +37,13 @@ def main(
     )
 
     for dialog, result in zip(dialogs, results):
-        for msg in dialog:
-            print(f"{msg['role'].capitalize()}: {msg['content']}\n")
-        print(
-            f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
-        )
-        print("\n==================================\n")
-
+        # for msg in dialog:
+        #     print(f"{msg['role'].capitalize()}: {msg['content']}\n")
+        # print(
+        #     f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+        # )
+        # print("\n==================================\n")
+        print("ImmiResult:",result['generation']['content'].strip())
 
 if __name__ == "__main__":
     fire.Fire(main)
